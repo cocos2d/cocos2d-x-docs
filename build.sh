f90fa15c59befeee07bcd11f63ea29e4dc17b5e5
Slack-Moehrle <slackmoehrle@users.noreply.github.com>
2017-11-27 02:12:00 -0500

    New docs #17 (#58)
    * started build script
    
    * edited format for more links. waiting on video creator for a static link
    
    * static index pages, used for re-direct and landing pages

--- build.sh
+++ build.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-# pull latest from github
+# This script builds: Cocos2d-x-docs, Cocos Creator Manual, Cocos Creator API-Ref.
+
+# First Cocos2d-x docs, we are aready in this repo when executing this script
+## pull latest from github
 git pull origin master
 
-# build it
+## build it
 gitbook build
 
-# copy everything to deployment directory
+## copy everything to deployment directory
 cd _build
 cp en/ gitbook/ package.json search_plus_index.json zh/ ../../documentation
 cd ..
 
-# Api-ref
+## Api-ref, we only have this as a .tar.gz file.
+cp ../api-refs-static-pages.tar.gz .
+tar xvf api-refs-static-pages.tar.gz
+rm -rf api-refs-static-pages.tar.gz
+
+
+
 
 # ensure index.html redirect
