5c5a063d7bd1ce3b581da1ba93c065ba45a5f1e0
Jason T Slack-Moehrle <slackmoehrle@gmail.com>
2017-12-04 01:50:32 -0500

    deployment to cocos2d-x dir

--- build.sh
+++ build.sh
@@ -31,11 +31,15 @@ echo "Cocos2d-x-docs -- removing unneeded files..."
 rm -rf api-refs-static-pages.tar.gz
 rm -rf editors_and_tools.tar.gz
 
-echo "Cocos2d-x-docs -- copy everything to deployment directory..."
-cp -R api-ref editors_and_tools en gitbook index.html package.json search_plus_index.json zh ../../documentation/
-
 cd ..
-rm -rf _book/
+
+mv _book/ cocos2d-x/
+mv crocos2d-x ../documentation/
+
+#echo "Cocos2d-x-docs -- copy everything to deployment directory..."
+#cp -R api-ref editors_and_tools en gitbook index.html package.json #search_plus_index.json zh ../../documentation/
+
+rm -rf cocos2d-x/
 
 echo "-----------------------"
 echo "Cocos Creator Manual..."
