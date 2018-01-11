9efcd342f2022b680da8f0605d4af72c09234738
Jare Guo <jareguo@users.noreply.github.com>
2018-01-11 19:38:29 +0800

    added step to copy config (#110)

--- build.sh
+++ build.sh
@@ -51,7 +51,9 @@ echo "-----------------------"
 ## pull latest from github
 echo "Cocos Creator -- pulling latest from GitHub..."
 cd ../creator-docs
-git pull origin cocos2d-x.org
+git pull origin master
+
+cp -rf config/cocos2d-x.org/. ./
 
 ## make sure gitbook is up to date
 echo "Cocos Creator -- ensuring GitBook is up to date..."
@@ -80,7 +82,9 @@ echo "------------------------"
 ## pull latest from github
 echo "Cocos Creator API -- pulling latest from GitHub..."
 cd ../creator-api-docs
-git pull origin cocos2d-x.org
+git pull origin master
+
+cp -rf config/cocos2d-x.org/. ./
 
 ## make sure gitbook is up to date
 echo "Cocos Creator API -- ensuring GitBook is up to date..."
