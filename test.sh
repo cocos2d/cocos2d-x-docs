81b564dc2aee59b920730c6a9e9837d8a63f3937
Jason Slack-Moehrle <slackmoehrle@gmail.com>
2019-05-28 20:34:33 -0400

    testing

--- test.sh
+++ test.sh
@@ -10,6 +10,7 @@ echo "----------------------"
 echo "Hosekeeping..."
 git_legacy=0
 gitv1=2.7.4
+git_string=0
 
 git_version="$(git --version)"
 git_version=${git_version#"git version "}
@@ -17,9 +18,12 @@ echo "Git version: "$git_version
 
 if [ "$(convert_to_integer $git_version)" -gt "$(convert_to_integer $gitv1)" ];then
     #echo "$git_version is greater than or equal to $gitv1"
+    git_legacy=0
+    git_string="Already up to date"
     echo "Git version: modern"
 else
     git_legacy=1
+    git_string="Already up-to-date"
     echo "Git version: legacy"
 fi
 
@@ -30,71 +34,34 @@ echo "Cocos2d-x-docs -- pulling latest from GitHub..."
 echo "-----------------------------------------------"
 git checkout master
 
-## git 2.7.4 returns different message than later versions
-if [ $git_legacy -eq 1 ]; then
-    echo "Git version: legacy"
-    if (git pull origin master | grep -q "Already up-to-date"); then
-        echo "Cocos2d-x skipped, GitHub repo up to date..."
-    else
-        echo "Building Cocos2d-x Docs..."
-
-        cp -rf config/cocos2d-x.org/. ./
-
-        ## make sure gitbook is up to date
-        echo "Cocos2d-x-docs -- ensuring GitBook is up to date..."
-        gitbook install
-
-        ## build it
-        echo "Cocos2d-x-docs -- building GitBook docs...."
-        gitbook build
-
-        ## copy some needed files
-        echo "Cocos2d-x-docs -- copying needed files..."
-        cp redirect.html.en _book/index.html
-        cp index.html.en ../documentation/index.html
-
-        ## copy everything to deployment directory
-        echo "Cocos2d-x-docs -- copy everything to deployment directory..."
-
-        mv _book/ cocos2d-x/
-
-        rsync --recursive cocos2d-x ../documentation
-
-        rm -rf cocos2d-x/
-
-        git stash
-    fi
+if (git pull origin master | grep -q "$git_string"); then
+    echo "Cocos2d-x skipped, GitHub repo up to date..."
 else
-    echo "Git version: modern"
-    if (git pull origin master | grep -q "Already up to date"); then
-        echo "Cocos2d-x skipped, GitHub repo up to date..."
-    else
-        echo "Building Cocos2d-x Docs..."
+    echo "Building Cocos2d-x Docs..."
 
-        cp -rf config/cocos2d-x.org/. ./
+    cp -rf config/cocos2d-x.org/. ./
 
-        ## make sure gitbook is up to date
-        echo "Cocos2d-x-docs -- ensuring GitBook is up to date..."
-        gitbook install
+    ## make sure gitbook is up to date
+    echo "Cocos2d-x-docs -- ensuring GitBook is up to date..."
+    gitbook install
 
-        ## build it
-        echo "Cocos2d-x-docs -- building GitBook docs...."
-        gitbook build
+    ## build it
+    echo "Cocos2d-x-docs -- building GitBook docs...."
+    gitbook build
 
-        ## copy some needed files
-        echo "Cocos2d-x-docs -- copying needed files..."
-        cp redirect.html.en _book/index.html
-        cp index.html.en ../documentation/index.html
+    ## copy some needed files
+    echo "Cocos2d-x-docs -- copying needed files..."
+    cp redirect.html.en _book/index.html
+    cp index.html.en ../documentation/index.html
 
-        ## copy everything to deployment directory
-        echo "Cocos2d-x-docs -- copy everything to deployment directory..."
+    ## copy everything to deployment directory
+    echo "Cocos2d-x-docs -- copy everything to deployment directory..."
 
-        mv _book/ cocos2d-x/
+    mv _book/ cocos2d-x/
 
-        rsync --recursive cocos2d-x ../documentation
+    rsync --recursive cocos2d-x ../documentation
 
-        rm -rf cocos2d-x/
+    rm -rf cocos2d-x/
 
-        git stash
-    fi
+    git stash
 fi
